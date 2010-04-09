5c040d08efc0aea38399a907a7f3493c9449aa2f
root <root@phoenix.tyo.ro>
2010-04-09 13:35:44 +0900

    コマンドの登録回りのリファクタ及び、SmartIRC_JPの利用への切り替え。 ranbatボットのバグ修正等と汎用機能のURL展開にてyotuubeやニコ動APIを使用する形に変更。 またURL展開の基礎部分のフローも見直し。

--- bot.php
+++ bot.php
@@ -11,67 +11,36 @@ include_once "./bot.conf.inc";
 include_once "./conf/conf.inc";
 include_once "./bot/common.class.inc";
 include_once "./bot/ranbat.class.inc";
-include_once "Net/SmartIRC.php";
+include_once "./bot/dani.class.inc";
+include_once "./include/SmartIRC_JP.php";
+
+$conn =& ADONewConnection('mysql');
+$conn->PConnect(DATABASE_HOST, DATABASE_ID, DATABASE_PASS, DATABASE_NAME);
+$conn->Execute('set names utf8');
 
 $bot_c = &new tyobot_common();
-$bot_r = &new tyobot_ranbat();
-$irc = &new Net_SmartIRC();
+$bot_r = &new tyobot_ranbat( $conn );
+$bot_d = &new tyobot_dani( $conn );
+
+$irc = &new Net_SmartIRC_JP();
 $irc->setDebug(SMARTIRC_DEBUG_ALL);
 $irc->setUseSockets(TRUE);
 $irc->setAutoReconnect(TRUE);
+$irc->setCharset( IRC_ENCODING );
 
-// BOT を終了する
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^' . BOT_NICK . DELIMITER . COMMAND_QUIT, $bot_c, COMMAND_QUIT);
-
-// URLのタイトルを表示
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, URL_PATTERN , $bot_c, COMMAND_URL );
-
-// Google検索
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^'. GOOGLE_CMD . ' ', $bot_c, COMMAND_GOOGLE );
-
-// 計算機機能
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^'. CALCULATOR_CMD . ' ', $bot_c, COMMAND_CALCULATOR ); 
-
-//twitter
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^'. TWITTER_CMD . ' ', $bot_c, COMMAND_TWITTER );
-
-// inviteされた
-$irc->registerActionhandler(SMARTIRC_TYPE_INVITE, '.*', $bot_c, '__invited__' );
-
-// amazon
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^'. AMAZON_CMD . ' ', $bot_c, COMMAND_AMAZON );
+$bot_c->_setCommand( $irc );
+$bot_r->_setCommand( $irc );
+$bot_d->_setCommand( $irc );
 
 $channels[] = $bot_c->_convert(CHANNEL);
-
-// ustream
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^'. USTREAM_CMD .' ', $bot_c, COMMAND_USTREAM );
-
-// ランバト関連
 $channels[] = $bot_r->_convert(RANBAT_CHANNEL);
-
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^'. RANBAT_ENTRY_CMD.' ' , $bot_r, COMMAND_RANBAT_ENTRY );
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^'. RANBAT_ENTRY_CMD_S.' ' , $bot_r, COMMAND_RANBAT_ENTRY );
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^'. $bot_r->_convert(RANBAT_ENTRY_ALIAS_CMD), $bot_r, COMMAND_RANBAT_ENTRY_ALIAS );
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^'. RANBAT_CANCEL_CMD.' ' , $bot_r, COMMAND_RANBAT_CANCEL );
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^'. RANBAT_CANCEL_CMD_S.' ' , $bot_r, COMMAND_RANBAT_CANCEL );
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^'. RANBAT_START_CMD , $bot_r, COMMAND_RANBAT_START );
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^'. RANBAT_LIST_CMD , $bot_r, COMMAND_RANBAT_LIST );
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^'. RANBAT_WIN_CMD.' ' , $bot_r, COMMAND_RANBAT_WIN );
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^'. RANBAT_WIN_CMD_S.' ' , $bot_r, COMMAND_RANBAT_WIN );
-
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^'. KOHAKU_ENTRY_CMD, $bot_r, COMMAND_KOHAKU_ENTRY );
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^'. KOHAKU_LEADER_CMD.' ' , $bot_r, COMMAND_KOHAKU_LEADER );
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^'. KOHAKU_LEADER_CMD_S.' ' , $bot_r, COMMAND_KOHAKU_LEADER );
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^'. KOHAKU_SELECT_CMD.' ' , $bot_r, COMMAND_KOHAKU_SELECT );
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^'. KOHAKU_SELECT_CMD_S.' ' , $bot_r, COMMAND_KOHAKU_SELECT );
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^'. KOHAKU_ADD_CMD.' ' , $bot_r, COMMAND_KOHAKU_ADD );
+$channels[] = $bot_r->_convert(DANI_CHANNEL);
 
 // test
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^!entry_start', $bot_r, 'entry_start' );
 $irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^!gowasu', $bot_r, 'gowasu' );
 $irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^!dosukoi ', $bot_r, 'dosukoi' );
 $irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^!dosukoi_rank',$bot_r, 'dosukoi_rank' );
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, 'gosama',$bot_r,'gosama');
+$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, 'ｺﾞｻﾏ', $bot_r,'gosama');
 
 $irc->connect(SERVER_HOST, SERVER_PORT);
 $irc->login(BOT_NICK, BOT_NAME);
